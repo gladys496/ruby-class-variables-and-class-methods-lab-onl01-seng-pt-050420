21560ab05b9d591804762f0cbc1aa65c6983ec25
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-15 03:43:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ class Song
  attr_accessor :name, :artist, :genre
   
   @@count = 0 
+  @@genres = []
   
   def initialize(name, artist, genre)
     @@count += 1 
@@ -13,6 +14,10 @@ class Song
  def self.count
     @@count
   end
+  
+  def self.genres 
+    @@genres.uniq! 
+  end 
  
  end 
  
