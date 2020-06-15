f1d85e98b3fe915b347613c033cd4ae0cc331572
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-15 05:09:04 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,57 @@
+class Song
+  
+   @@count = 0 
+  @@genres = []
+  @@artists = []
+  
+  def self.count 
+  @@count
+end 
+
+def self.artists
+  @@artists.uniq 
+end 
+
+def self.genres 
+  @@genres.uniq 
+end 
+   def self.genre_count 
+    genre_hash = {} 
+    @@genres.each do |genre|
+      if genre_hash[genre]
+        genre_hash[genre] += 1
+      else 
+        genre_hash[genre] = 1
+        end 
+      end 
+      genre_hash
+    end 
+    
+    def self.artist_count
+      artist_hash = {}
+      @@artists.each {|artist|
+      artist_hash[artist] ||= 0 
+      artist_hash[artist] += 1 
+      }
+
+      artist_hash
+    end 
+ 
+  
+   attr_accessor :name, :artist, :genre
+ 
+ 
+  def initialize(name, artist, genre)
+    @@count += 1 
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@artists << artist 
+    @@genres << genre 
+  end 
+end 
+  
+    
+
+  
+
