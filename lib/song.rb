071beac4a694293b6c35f2cf2029701eebef17e8
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-15 03:20:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,32 +3,16 @@ class Song
 
   attr_accessor :name
   attr_accessor :artist
-  attr_accessor :duration
+  attr_accessor :genre
   
-  def initialize(name, artist, duration)
+  def initialize(name, artist, genre)
     @name = name
     @artist = artist
-    @duration = duration
-	@plays = 0
-  end
-  
-  def duration_in_minutes
-    @duration / 60.0
-  end
-  
-  def duration_in_minutes=(new_duration)
-    @duration = (new_duration * 60).to_i
-  end
-  
-  def play
-    @plays += 1
-	@@plays += 1
-	puts "The song: #@plays plays. Total #@@plays plays."
-  end
+    @genre = genre
 
-  def to_s
-    "Song: #@name--#@artist (#@duration)"
   end
+  
+  
 end
 
 
