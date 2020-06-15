cb08278b64199ceeb45fcc626ab8d06557de2721
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-15 04:59:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,17 @@ end
 def self.genres 
   @@genres.uniq 
 end 
-  
+   def self.genre_count 
+    genre_hash = {} 
+    @@genres.each do |genre|
+      if genre_hash[genre]
+        genre_hash[genre] += 1
+      else 
+        genre_hash[genre] = 1
+        end 
+      end 
+      genre_hash
+    end 
  
   
    attr_accessor :name, :artist, :genre
