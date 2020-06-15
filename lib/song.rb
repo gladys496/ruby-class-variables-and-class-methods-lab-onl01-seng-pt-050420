0a357bbe6db2193189c7c63bd2b2a32a47a8d345
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-15 04:20:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,8 +21,13 @@ class Song
     @@genres 
   end 
   
-  
-  
+  def genres
+    self.songs.collect{|s| s.genre}.uniq
+  end 
+  def self.create(artist)
+    artist = Artist.new(artist) 
+    artist.save
+    artist
  
  end 
  
